27d0bc5807d82790f9ad3674e9f889f5de1877fb
toremann <50668920+toremann@users.noreply.github.com>
2022-03-27 23:33:29 +0200

    clean up

--- app/app.js
+++ app/app.js
@@ -3,48 +3,43 @@ const fs = require("fs");
 
 (async () => {
   const player = "Toremann";
-
   const server = "Stormscale";
+
+ 
 
   const apiURL = "https://check-pvp.fr/api/characters/eu/";
 
-  let amountOfAlts = 0;
-
-  let currentAlt = 0;
-
   let dataResponse = [];
+
+  // Alt's data for new axios request
   let dataResponseAlts = [];
 
   // AXIOS HER?
 
   axios.put(apiURL + server + "/" + player + "/battlenet").then((response) => {
     dataResponse.push(response.data);
-    dataResponseAlts.push(response.data.rerolls);
+    dataResponseAlts.push(JSON.stringify(response.data.rerolls, ['name', 'realm'], 5))
 
-     // Set amout of alts
-     amountOfAlts = Object.keys(response.data.rerolls).length;
+    console.log('Alts:', dataResponseAlts)
 
+    let altPlayer = dataResponseAlts.name;
+    let altPlayerServer = dataResponseAlts.realm;
     // console.log("DataResponse:", dataResponse);
     // console.log("DataResponseAlts:", dataResponseAlts);
     // console.log(amountOfAlts);
-    console.log(JSON.stringify(dataResponse, ['name', 'realm', 'rateatm2v2', 'rateatm3v3', 'rateatmrbg', 'rerolls'], 1))
+    // console.log(JSON.stringify(dataResponse, ['name', 'realm', 'rateatm2v2', 'rateatm3v3', 'rateatmrbg', 'rerolls'], 1))
 
-    // for (; currentAlt < amountOfAlts; currentAlt++) {
-    //   console.log(
-    //     "Alt:",
-    //     Object.entries(response.data.rerolls[currentAlt]).slice(0, 2),
-    //     "Rating:",
-    //     Object.entries(response.data.rerolls[currentAlt]).slice(8, 10)
-    //   );
-    // }
-
+    // Find information I want
     let dataStringfy = JSON.stringify(dataResponse, ['name', 'realm', 'rateatm2v2', 'rateatm3v3', 'rateatmrbg', 'rerolls'], 1);
 
+    console.log(altPlayer, altPlayerServer)
+
+    // Write data from respons to data.json
     let test = fs.writeFile("data.json", dataStringfy, function (err) {
       if (err) {
         console.log(err);
       } else {
-        console.log("Success");
+        console.log("Wrote to data.json");
       }
     });
   });
