7a1a806be7848e5e45b7458b90e6f1f2b87228c7
toremann <50668920+toremann@users.noreply.github.com>
2022-03-27 23:08:05 +0200

    push to json

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 const axios = require("axios").default;
+const fs = require("fs");
 
 (async () => {
   const player = "Toremann";
@@ -11,33 +12,40 @@ const axios = require("axios").default;
 
   let currentAlt = 0;
 
+  let dataResponse = [];
+  let dataResponseAlts = [];
+
   // AXIOS HER?
 
   axios.put(apiURL + server + "/" + player + "/battlenet").then((response) => {
-    // Set amout of alts
-    amountOfAlts = Object.keys(response.data.rerolls).length;
+    dataResponse.push(response.data);
+    dataResponseAlts.push(response.data.rerolls);
 
-    console.log(amountOfAlts);
-    console.log(
-      "Main:",
-      response.data.name,
-      "-",
-      response.data.realm,
-      "2v2:",
-      response.data.rateatm2v2,
-      "3v3:",
-      response.data.rateatm3v3,
-      "RBG:",
-      response.data.rateatmrbg
-    );
+     // Set amout of alts
+     amountOfAlts = Object.keys(response.data.rerolls).length;
 
-    for (; currentAlt < amountOfAlts; currentAlt++) {
-      console.log(
-        "Alt:",
-        Object.entries(response.data.rerolls[currentAlt]).slice(0, 2),
-        "Rating:",
-        Object.entries(response.data.rerolls[currentAlt]).slice(8, 10)
-      );
-    }
+    // console.log("DataResponse:", dataResponse);
+    // console.log("DataResponseAlts:", dataResponseAlts);
+    // console.log(amountOfAlts);
+    console.log(JSON.stringify(dataResponse, ['name', 'realm', 'rateatm2v2', 'rateatm3v3', 'rateatmrbg', 'rerolls'], 1))
+
+    // for (; currentAlt < amountOfAlts; currentAlt++) {
+    //   console.log(
+    //     "Alt:",
+    //     Object.entries(response.data.rerolls[currentAlt]).slice(0, 2),
+    //     "Rating:",
+    //     Object.entries(response.data.rerolls[currentAlt]).slice(8, 10)
+    //   );
+    // }
+
+    let dataStringfy = JSON.stringify(dataResponse, ['name', 'realm', 'rateatm2v2', 'rateatm3v3', 'rateatmrbg', 'rerolls'], 1);
+
+    let test = fs.writeFile("data.json", dataStringfy, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Success");
+      }
+    });
   });
 })();
