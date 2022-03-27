17ee9b47dc1894c354ed2d59ac18c30f4b340404
toremann <50668920+toremann@users.noreply.github.com>
2022-03-27 21:03:28 +0200

    changed to axios

--- app/app.js
+++ app/app.js
@@ -1,67 +1,43 @@
-const { chromium } = require("playwright");
+const axios = require("axios").default;
 
 (async () => {
-  const browser = await chromium.launch({
-    // Set headless to false to launch chromium and see the script work
-    headless: true,
-  });
+  const player = "Toremann";
 
-  // Players to check
-  const player = [
-    "Toremann",
-    "Totemtore",
-    "Tutoress",
-    "Luksustore",
-    "Torebukk",
-  ];
-  // Player server
-  const server = "Stormscale/";
-  const url = "https://check-pvp.fr/eu/";
+  const server = "Stormscale";
 
-  // Vars for loop
-  let playerLength = player.length;
-  let players = 0;
+  const apiURL = "https://check-pvp.fr/api/characters/eu/";
 
-  const context = await browser.newContext();
+  let amountOfAlts = 0;
 
-  // Open new page
-  const page = await context.newPage();
+  let currentAlt = 0;
 
-  // Array for ratings (2v2, 3v3 and RBG)
-  const ratingArray = [];
+  // AXIOS HER?
 
-  // The loop
-  console.log("Updating", player.length, "players");
-  console.log("---------------------");
-  for (; players < playerLength; players++) {
-    await page.goto(url + server + player[players]);
-    console.log("Player:", player[players], "-", server);
+  axios.put(apiURL + server + "/" + player + "/battlenet").then((response) => {
+    // Set amout of alts
+    amountOfAlts = Object.keys(response.data.rerolls).length;
 
-    // Get the rating
-    const findRating = await page.$$(".cote-atm-value");
+    console.log(amountOfAlts);
+    console.log(
+      "Main:",
+      response.data.name,
+      "-",
+      response.data.realm,
+      "2v2:",
+      response.data.rateatm2v2,
+      "3v3:",
+      response.data.rateatm3v3,
+      "RBG:",
+      response.data.rateatmrbg
+    );
 
-    for (const p of findRating) {
-      const ratingTrimmed = await page.evaluate((el) => el.innerText.trim(), p);
-      // Push output from ratingTrimmed to ratingArray
-      ratingArray.push(ratingTrimmed);
+    for (; currentAlt < amountOfAlts; currentAlt++) {
+      console.log(
+        "Alt:",
+        Object.entries(response.data.rerolls[currentAlt]).slice(0, 2),
+        "Rating:",
+        Object.entries(response.data.rerolls[currentAlt]).slice(8, 10)
+      );
     }
-
-    console.log("2v2 rating:", ratingArray[0]);
-    console.log("3v3 rating:", ratingArray[1]);
-    console.log("RBG rating:", ratingArray[2]);
-
-    // Clear array and loop again with empty array
-    ratingArray.length = 0;
-
-    // Update the player
-    await page.locator("text=Update now").click();
-    console.log("Updating:", player[players], "-", server);
-    await page.locator("text=Yes").click();
-    console.log("Update success!");
-    console.log("---------------------");
-  }
-
-  await context.close();
-  await browser.close();
-  console.log("\x1b[33m%s\x1b[0m", "Update finished :)");
+  });
 })();
