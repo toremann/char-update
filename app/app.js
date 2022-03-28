9b2b4943cff1ce6d11dda87edc9fb5bacb66eca9
toremann <50668920+toremann@users.noreply.github.com>
2022-03-28 13:07:44 +0200

    create and push urls to arrayOfPromisess

--- app/app.js
+++ app/app.js
@@ -4,9 +4,6 @@ const fs = require("fs");
 (async () => {
   const player = "Toremann";
   const server = "Stormscale";
-
- 
-
   const apiURL = "https://check-pvp.fr/api/characters/eu/";
 
   let dataResponse = [];
@@ -14,25 +11,39 @@ const fs = require("fs");
   // Alt's data for new axios request
   let dataResponseAlts = [];
 
+  // Array of urls: https://check-pvp.fr/api/characters/eu/REALM/NAME/battlenet
+  let arrayOfPromisess = [];
+
   // AXIOS HER?
 
   axios.put(apiURL + server + "/" + player + "/battlenet").then((response) => {
     dataResponse.push(response.data);
-    dataResponseAlts.push(JSON.stringify(response.data.rerolls, ['name', 'realm'], 5))
+    dataResponseAlts.push(
+      JSON.stringify(response.data.rerolls, ["name", "realm"])
+    );
+    // FILL arrayOfPromisess
+    arrayOfPromisess.push(
+      apiURL +
+        dataResponseAlts[(0, 1)] +
+        "/" +
+        dataResponseAlts[(0, 2)] +
+        "/battlenet"
+    );
+    console.log("array of promisess:", arrayOfPromisess);
 
-    console.log('Alts:', dataResponseAlts)
+    // axios.put(apiURL + dataResponseAlts.server + "/" + dataResponseAlts.name + "/battlenet").then((response) => {
+    //   dataReponseAltsRating.push(JSON.stringify(response))
+    // }
+    // )
 
-    let altPlayer = dataResponseAlts.name;
-    let altPlayerServer = dataResponseAlts.realm;
-    // console.log("DataResponse:", dataResponse);
-    // console.log("DataResponseAlts:", dataResponseAlts);
-    // console.log(amountOfAlts);
-    // console.log(JSON.stringify(dataResponse, ['name', 'realm', 'rateatm2v2', 'rateatm3v3', 'rateatmrbg', 'rerolls'], 1))
+    // Find information I want for main char
+    let dataStringfy = JSON.stringify(
+      dataResponse,
+      ["name", "realm", "rateatm2v2", "rateatm3v3", "rateatmrbg", "rerolls"],
+      1
+    );
 
-    // Find information I want
-    let dataStringfy = JSON.stringify(dataResponse, ['name', 'realm', 'rateatm2v2', 'rateatm3v3', 'rateatmrbg', 'rerolls'], 1);
-
-    console.log(altPlayer, altPlayerServer)
+    console.log("dataresponsealts:", dataResponseAlts);
 
     // Write data from respons to data.json
     let test = fs.writeFile("data.json", dataStringfy, function (err) {
